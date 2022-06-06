f61bb7e8f036a30dfdefcb82695bd3278b9fbbbf
qiujie.wang <wangqj799@163.com>
2022-06-06 22:26:36 +0800

    Init

--- src/main/java/ex01/pyrmont/HttpServer.java
+++ src/main/java/ex01/pyrmont/HttpServer.java
@@ -2,8 +2,11 @@ package ex01.pyrmont;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class HttpServer {
     public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webapp";
@@ -19,12 +22,38 @@ public class HttpServer {
         ServerSocket serverSocket = null;
         int port = 8080;
 
-        try{
+        try {
             serverSocket = new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
-            
-        }catch(IOException e) {
+
+        } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
+        }
+
+        while (!shutdown) {
+            Socket socket = null;
+            InputStream input = null;
+            OutputStream output = null;
+
+            try {
+                socket = serverSocket.accept();
+                input = socket.getInputStream();
+                output = socket.getOutputStream();
+
+                Request request = new Request(input);
+                request.parse();
+
+                Response response = new Response(output);
+                response.setRequest(request);
+                response.sendStaticResource();
+
+                socket.close();
+                shutdown = request.getUri().equals(SHUTDOWN_COMMAND);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
         }
     }
 }
